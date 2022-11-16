66f0c8369ed9e64a0a344ef60415aa1863957721
Arham Abeer Ahmed <57269012+arhamabeer@users.noreply.github.com>
2022-11-16 14:18:23 +0500

    delete file

--- controller/File.ts
+++ controller/File.ts
@@ -1,3 +1,4 @@
+import { response } from "express";
 import FileModel from "../model/FileModel";
 
 // SAVE
@@ -24,10 +25,24 @@ const SaveFile = async (req: any, res: any) => {
 const FecthFile = async (req: any, res: any) => {
   try {
     let response = await FileModel.find({ room_id: req.body.room_id });
-    res.status(200).send({ msg: "New File Added!", result: response });
+    res.status(200).send({ msg: "File Fetched!", result: response });
   } catch (error) {
     res.status(500).send({ msg: "Internal Server Error", error: error });
   }
 };
 
-export { SaveFile, FecthFile };
+// DELETE
+const DeleteFile = async (req: any, res: any) => {
+  const { file_id, author_id } = req.body;
+  try {
+    let response = await FileModel.deleteOne({
+      author_id: author_id,
+      _id: file_id,
+    });
+    res.status(200).send({ msg: "File Deleted!", result: response });
+  } catch (error) {
+    res.status(500).send({ msg: "Internal Server Error", error: error });
+  }
+};
+
+export { SaveFile, FecthFile, DeleteFile };
--- router/FileRouter.ts
+++ router/FileRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { SaveFile, FecthFile } from "../controller/File";
+import { SaveFile, FecthFile, DeleteFile } from "../controller/File";
 const router = express.Router();
 
 router.post("/saveFile", SaveFile);
 router.get("/getFile", FecthFile);
+router.delete("/deleteFile", DeleteFile);
 
 export default router;
