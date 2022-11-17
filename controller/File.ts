30bcd368a36818b6de50a692ba00000a389fbb96
Arham Abeer Ahmed <57269012+arhamabeer@users.noreply.github.com>
2022-11-17 23:56:49 +0500

    new

--- controller/File.ts
+++ controller/File.ts
@@ -3,18 +3,19 @@ import FileModel from "../model/FileModel";
 
 // SAVE
 const SaveFile = async (req: any, res: any) => {
+  console.log(req.body);
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  } else {
-    try {
-      let data = new FileModel({ ...req.body });
-      let response = await data.save();
-      res.status(200).send({ msg: "New File Added!", result: response });
-    } catch (error) {
-      res.status(500).send({ msg: "Internal Server Error", error: error });
-    }
-  }
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json({ errors: errors.array() });
+  // } else {
+  //   try {
+  //     let data = new FileModel({ ...req.body });
+  //     let response = await data.save();
+  //     res.status(200).send({ msg: "New File Added!", result: response });
+  //   } catch (error) {
+  //     res.status(500).send({ msg: "Internal Server Error", error: error });
+  //   }
+  // }
 };
 
 // FETCH
