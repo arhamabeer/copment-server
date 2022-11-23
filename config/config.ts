6f28008845503be371e42e563ebc53650acca81c
Arham Abeer Ahmed <57269012+arhamabeer@users.noreply.github.com>
2022-11-23 17:30:10 +0500

    db changes

--- config/config.ts
+++ config/config.ts
@@ -5,13 +5,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db_uri = process.env.MDB_URI || "";
+// const db_uri = process.env.MDB_URI || "";
 
 export default function Config(server: Express) {
-  mongoose
-    .connect(db_uri, { dbName: "Copment-server" })
-    .then(() => console.log("db connected...."))
-    .catch((e) => console.log("db not connected...", e));
+  process.env.MDB_URI &&
+    mongoose
+      .connect(process.env.MDB_URI, { dbName: "Copment-server" })
+      .then(() => console.log("db connected...."))
+      .catch((e) => console.log("db not connected...", e));
 
   server.use("/", router);
 }
